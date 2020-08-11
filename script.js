c97691a1e9ebde55190cccc8731f1ea8e9f53348
HersinJawad <muhammadhersinjawad06061998.com>
2020-08-11 16:00:45 +0700

    learn day 3

---
+++ script.js
@@ -0,0 +1,7 @@
+//window.alert("Hersin Ganteng")
+//window.prompt("khairul Jelek katakan YA")
+
+document.getElementById('element')
+console.log(document.getElementById('element'))
+
+document.body.innerHTML += "<h1>Hello World</h1>" 
